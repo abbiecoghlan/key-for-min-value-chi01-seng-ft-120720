56ae5abbd0ed09bfa2eacc277242ced55b218763
Abbie Coghlan <abbie.coghlan@gmail.com>
2020-11-20 19:23:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,10 @@ def key_for_min_value(name_hash)
 
 
   name_hash.each do |key, value|
-    if lowest_value == nil
+    if lowest_value == NIL
       lowest_value = value
       min_key = key
-    elsif lowest_value != nil
+    elsif lowest_value != NIL
       if lowest_value > value
         lowest_value == value
         min_key = key
